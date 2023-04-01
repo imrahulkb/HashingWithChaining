5c75c9eb4f8324038962107a423625031de5122d
imrahulkb <therahulrrj007@gmail.com>
2023-04-02 02:09:16 +0530

    Changes in Code and information in readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# HashingWithChaining
+# Hashing With Chaining
+### This program demonstrates the algorith of hashing using chaining. There are 10 buckets considered in this program. You can modify this code to increase the number of buckets.
+
+## PULL REQUESTS
+### Pull requests which can make the code better to understand and also improve the UX are appreciated.
--- code.c
+++ code.c
@@ -1,50 +1,47 @@
 #include <stdio.h>
 #include<stdlib.h>
-typedef struct
+struct link
 {
     int data;
     struct link* next;
-} link;
+};
+typedef struct link link;
 
 link* newNode(int data)
 {
-    link * new;
-    new=(link *)malloc(sizeof(link));
+    link * new = (link *)malloc(sizeof(link));
     new->data=data;
     return new;
 }
 
 link* insert(int data,link* head)
 {
-    if(head==NULL)
-    return newNode(data);
-    else
-    head->next=insert(data,head->next);
+    if(head==NULL) return newNode(data);
+    else head->next=insert(data,head->next);
     return head;
 }
 
 link* display(link* head)
 {
-    if(head!=NULL)
+    link * temp = head;
+    while(head!=NULL)
     {
-        printf("%d->",head->data);
-        display(head->next);
+        printf("%d->",temp->data);
+        temp=temp->next;
     }
 }
 
-link*search(int data, link*head)
+link* search(int data, link* head)
 {
    while(head!=NULL)
    {
        if(head->data==data)
        {
-         //  printf("Data Found At address : %p",head);
            return head;
            exit(0);
        }
        head=head->next;
    }
-  // printf("Given data not found : ");
    return NULL;
 }
 
@@ -59,35 +56,33 @@ int main()
         switch(o)
         {
             case 1:
-            printf("\nEnter the integer to be inserted : ");
-            scanf("%d",&d);
-          //  printf("Data Scanned\n ");
-            x=d%10;
-         //   printf("Index calculated ");
-            if(arr[x]==NULL)
-            {
-                arr[x]=insert(d,arr[x]);
-            }
-            else insert(d,arr[x]);
-            break;
+                printf("\nEnter the integer to be inserted : ");
+                scanf("%d",&d);
+                x=d%10;
+                if(arr[x]==NULL)
+                {
+                    arr[x]=insert(d,arr[x]);
+                }
+                else insert(d,arr[x]);
+                break;
             case 2:
-            for(int i=0;i<10;i++)
-            {
-                printf("\n %d : ",i);
-                display(arr[i]);
-                printf("(nil)\n");
-            }
-           break;
-           case 3:
-           printf("Enter The Number To Be Searched in hash Table : ");
-           scanf("%d",&d);
-           x=d%10;
-           if(search(d,arr[x])!=NULL)
-           printf("\t\tData Found At address : %p",search(d,arr[x]));
-           else printf("\t\tGiven data not found : ");
-           break;
+                for(int i=0;i<10;i++)
+                {
+                    printf("\n %d : ",i);
+                    display(arr[i]);
+                    printf("(nil)\n");
+                }
+                break;
+            case 3:
+                printf("Enter The Number To Be Searched in hash Table : ");
+                scanf("%d",&d);
+                x=d%10;
+                if(search(d,arr[x])!=NULL)
+                printf("\t\tData Found At address : %p",search(d,arr[x]));
+                else printf("\t\tGiven data not found : ");
+                break;
             default:
-            printf("\t\tInvalid Input !!!");
+                printf("\t\tInvalid Input !!!");
         }
     }
     return 0;
